1ded9369813c1aadbee4a8baba3bd0b6c6647c9b
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-08-07 20:44:40 -0300

    style: ajustes nas imagens nos card de opções

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('.oferta, .card-opcao, .card-menu').forEach(function(card) {
+    card.addEventListener('click', function() {
+        window.location.href = './pages/contato.html';
+    });
+});
